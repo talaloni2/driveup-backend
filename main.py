1b8de59c794c358b4c509080cd15b08058c9a5ee
Tal Aloni <tal.aloni@planckresolution.com>
2023-04-17 07:26:47 +0300

    App is now runnable

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-import asyncio
-
 import uvicorn
 
 import server
 from component_factory import get_config
-from model.configuration import Config
-
-
-def main(config: Config):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
 
 if __name__ == "__main__":
-    main(get_config())
+    uvicorn.run(app=server.app, host="0.0.0.0", port=get_config().server_port)
