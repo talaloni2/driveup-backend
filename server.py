7333f7a28af734b7868946b10cb6e20721d74cf6
Tal Aloni <tal.aloni@planckresolution.com>
2023-05-07 21:52:18 +0300

    Added root router and started creating nginx

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from component_factory import get_user_handler_service
@@ -10,7 +10,7 @@ from controllers.geocoding import router as geocoding_router
 from service.user_handler_service import UserHandlerService
 
 app = FastAPI()
-
+root_router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
@@ -21,9 +21,9 @@ async def login(
 ):
     return await user_handler_service.login(username=form_data.username, password=form_data.password)
 
-
-app.include_router(rating_router, prefix="/rating", tags=["rating"])
-app.include_router(images_router, prefix="/images", tags=["images"])
-app.include_router(users_router, prefix="/users", tags=["users"])
-app.include_router(users_map_router, prefix="/users_map", tags=["users_map"])
-app.include_router(geocoding_router, prefix="/geocode", tags=["geocode"])
+root_router.include_router(rating_router, prefix="/rating", tags=["rating"])
+root_router.include_router(images_router, prefix="/images", tags=["images"])
+root_router.include_router(users_router, prefix="/users", tags=["users"])
+root_router.include_router(users_map_router, prefix="/users_map", tags=["users_map"])
+root_router.include_router(geocoding_router, prefix="/geocode", tags=["geocode"])
+app.include_router(root_router, prefix="/driveup")
