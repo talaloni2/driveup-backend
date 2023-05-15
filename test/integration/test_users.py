01426a1179b4d901aeba56f3c2d12b61f2ca1a63
Aviv Aloni <aviv@vulcan.io>
2023-05-15 23:27:14 +0300

    Fix users tests.

--- test/integration/test_users.py
+++ test/integration/test_users.py
@@ -16,26 +16,32 @@ async def test_create_login_get_update_delete(test_client_unauthenticated: TestC
         req_body=RequestUser(parameter=UserSchema(
             email=new_username,
             password="1234",
-            phone_number=get_random_string(),
-            full_name=get_random_string(),
+            phone_number=new_username,
+            full_name=new_username,
         )),
         resp_model=UserHandlerResponse
     )).result["token"]
-    assert (await test_client.get(
+    response = await test_client.get(
         url=f'/users/{new_username}',
         resp_model=UserHandlerResponse,
         headers={"Authorization": f"Bearer {token}"}
-    )) == UserHandlerResponse(code=200, status='OK', message='User fetched successfully', result={'email': new_username, 'phone_number': '0542583838', 'car_model': 'Hatzil', 'plate_number': '0000000', 'full_name': 'Dov Sherman', 'car_color': 'Black'}, detail=None)
-    assert (await test_client.put(
-        url='/users/user_for_test@gmail.com',
+    )
+    del response.result["password"]
+    assert response == UserHandlerResponse(code=200, status='OK', message='User fetched successfully', result={'full_name': new_username, 'email': new_username, 'car_color': None, 'phone_number': new_username, 'car_model': None, 'plate_number': None}, detail=None)
+    response = await test_client.put(
+        url=f'/users/update',
         req_body=RequestUser(parameter=UserSchema(
             full_name="Tznon Metoonaf"
         )),
         resp_model=UserHandlerResponse,
-    )) == UserHandlerResponse(code=200, status='OK', message='User updated successfully', result={'email': 'user_for_test@gmail.com', 'password': 'Aa111111', 'phone_number': '0542583838', 'car_model': 'Hatzil', 'plate_number': '0000000', 'token': '094ebeb0-a7be-5653-9752-16a1b22c688a', 'full_name': 'Tznon Metoonaf', 'car_color': 'Black'}, detail=None)
+        headers={"Authorization": f"Bearer {token}"}
+    )
+    del response.result["password"]
+    assert response == UserHandlerResponse(code=200, status='OK', message='User updated successfully', result={'full_name': 'Tznon Metoonaf', 'email': new_username, 'car_color': None, 'phone_number': new_username, 'car_model': None, 'plate_number': None}, detail=None)
     assert (await test_client.delete(
-        url='/users/user_for_test@gmail.com',
+        url='/users/delete',
         resp_model=UserHandlerResponse,
+        headers={"Authorization": f"Bearer {token}"}
     )) == UserHandlerResponse(code=200, status='OK', message='User deleted successfully', result=None, detail=None)
 
 
